d9f718fcbb650e5252deee9f17879a27ac079c2e
Joe Hitchens <joe@sleepless.com>
2014-01-07 22:15:54 -0800

    -

--- site/pull.php
+++ site/pull.php
@@ -3,6 +3,8 @@
 
 passthru("date");
 passthru("pwd");
-passthru("/usr/local/bin/git status -v");
+passthru("/usr/local/bin/git status");
+passthru("/usr/local/bin/git diff");
+passthru("/usr/local/bin/git commit -am -");
 
 ?>
